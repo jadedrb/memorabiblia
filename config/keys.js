d48d73f35a11c58eb26bb60e62f6ee93687720fc
Jade Del Rosario-Bell <JadeDRB@Jades-MBP.lan1>
2020-10-08 00:38:27 -0400

    git hub specifics

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://JadeDRB:3GVYr8IXKYvmv9Ym@cluster0-xbvbt.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    secretString: 'super secret string wow'
-}
---
+++ config/keys_sample.js
@@ -0,0 +1,6 @@
+/*
+module.exports = {
+    mongoURI: MONGO_URI_HERE,
+    secretString: JWT_SECRET_STRING_HERE
+}
+*/
