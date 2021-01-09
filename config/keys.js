576a9cf71a940d50a03d0414427f509960296c6e
Jade <whatever@gmail.com>
2021-01-09 15:18:41 -0500

    fixing bug where with a required file that was also ignored and failed to run

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-const keys_dev = require('./keys_dev')
+const keys_dev = process.env.MONGO_URI || require('./keys_dev')
+console.log(process.env.NODE_ENV)
 
 module.exports = {
     mongoURI: process.env.MONGO_URI || keys_dev.DEV_MONGO_URI,
