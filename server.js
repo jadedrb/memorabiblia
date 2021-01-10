d42f7372dd5412fd778705dd79532fa14493509a
Jade <jadecdrb@gmail.com>
2021-01-10 08:58:56 -0500

    messing with heroku variables

--- server.js
+++ server.js
@@ -27,6 +27,21 @@ mongoose
 app.use('/api/memories', memories);
 app.use('/api/users', users);
 
+// Test Route for Heroku env variables
+app.get('/heroku-env', (req, res) => {
+    let GBOOKS2 = process.env.GBOOKS_API 
+    let DEFINE2 = process.env.DEFINE_API
+    let NODE_ENV2 = process.env.NODE_ENV
+    res.status(200).json({
+        GBOOKS1: process.env.GBOOKS_API,
+        GBOOKS2,
+        DEFINE1: process.env.DEFINE_API,
+        DEFINE2,
+        NODE_ENV: process.env.NODE_ENV === undefined ? 'undefined' : process.env.NODE_ENV,
+        NODE_ENV2
+    })
+})
+
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
